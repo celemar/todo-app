123dff947f0852badfdae2715ce5d70f9ab1f5eb
celemar <celemar@pm.me>
2024-07-22 10:09:48 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
+import { Josefin_Sans } from '@next/font/google';
 import "./globals.css";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const josefinSans = Josefin_Sans({ weight: ['400', '700'] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={josefinSans.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
