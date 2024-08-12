f0e3c1c8a00dc26c4fb781fae96c975eb50ebb5a
celemar <celemar@pm.me>
2024-08-12 10:42:46 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main>
       <TodoList />
-      <p className="text-sm text-[#9394a5] dark:text-[#4d5067] mt-[10vh] md:mt-10 text-center">
+      <p className="text-sm text-[#9394a5] dark:text-[#4d5067] mt-[11vh] md:mt-10 text-center">
         Drag and drop to reorder list
       </p>
     </main>
