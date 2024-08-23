d6e1216cbc0146b959e3bdef0da32580195c5eba
celemar <celemar@pm.me>
2024-08-23 11:05:10 -0300

    update

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -25,16 +25,16 @@ import {
 
 import TodoItem from "./TodoItem/TodoItem";
 
-export default function TodoList() {
-  const defaultTodos: Todo[] = [
-    { id: 1, text: "Complete online JavaScript course", isCompleted: true },
-    { id: 2, text: "Jog around the park 3x", isCompleted: false },
-    { id: 3, text: "10 minutes meditation", isCompleted: false },
-    { id: 4, text: "Read for 1 hour", isCompleted: false },
-    { id: 5, text: "Pick up groceries", isCompleted: false },
-    { id: 6, text: "Complete Todo App on Frontend Mentor", isCompleted: false },
-  ];
+const defaultTodos: Todo[] = [
+  { id: 1, text: "Complete online JavaScript course", isCompleted: true },
+  { id: 2, text: "Jog around the park 3x", isCompleted: false },
+  { id: 3, text: "10 minutes meditation", isCompleted: false },
+  { id: 4, text: "Read for 1 hour", isCompleted: false },
+  { id: 5, text: "Pick up groceries", isCompleted: false },
+  { id: 6, text: "Complete Todo App on Frontend Mentor", isCompleted: false },
+];
 
+export default function TodoList() {
   const getStoredTodos = (): Todo[] => {
     const hasInitialized = localStorage.getItem("initialized");
     if (!hasInitialized) {
