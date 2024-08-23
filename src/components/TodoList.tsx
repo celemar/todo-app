93d88934b34e1dcbfd8ce746e2adb51ae8ee7bfd
celemar <celemar@pm.me>
2024-08-23 11:10:18 -0300

    update

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -25,16 +25,17 @@ import {
 
 import TodoItem from "./TodoItem/TodoItem";
 
-const defaultTodos: Todo[] = [
-  { id: 1, text: "Complete online JavaScript course", isCompleted: true },
-  { id: 2, text: "Jog around the park 3x", isCompleted: false },
-  { id: 3, text: "10 minutes meditation", isCompleted: false },
-  { id: 4, text: "Read for 1 hour", isCompleted: false },
-  { id: 5, text: "Pick up groceries", isCompleted: false },
-  { id: 6, text: "Complete Todo App on Frontend Mentor", isCompleted: false },
-];
-
 export default function TodoList() {
+  const defaultTodos: Todo[] = [
+    { id: 1, text: "Complete online JavaScript course", isCompleted: true },
+    { id: 2, text: "Jog around the park 3x", isCompleted: false },
+    { id: 3, text: "10 minutes meditation", isCompleted: false },
+    { id: 4, text: "Read for 1 hour", isCompleted: false },
+    { id: 5, text: "Pick up groceries", isCompleted: false },
+    { id: 6, text: "Complete Todo App on Frontend Mentor", isCompleted: false },
+  ];
+
+  
   const getStoredTodos = (): Todo[] => {
     const hasInitialized = localStorage.getItem("initialized");
     if (!hasInitialized) {
@@ -42,6 +43,7 @@ export default function TodoList() {
       localStorage.setItem("initialized", "true");
       return defaultTodos;
     }
+    
     const storedTodos = localStorage.getItem("todos");
     if (!storedTodos) {
       return [];
